b6fce4b463d77b508d983acbcd36de79ab67f35c
Primeway Skills Private Limited <primewaycollab@gmail.com>
2023-02-08 15:02:14 +0530

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:primewayskills_app/controllers/notification_controller.dart';
 import 'package:primewayskills_app/controllers/route_controller.dart';
 import 'package:primewayskills_app/view/helpers/colors.dart';
